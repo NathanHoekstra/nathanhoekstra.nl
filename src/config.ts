35a8b0cb77016b69366c62065c0975c27a5bea83
Nathan Hoekstra <dev@nathanhoekstra.nl>
2026-01-12 17:07:57 +0100

    Update to astro-paper v5.5.1

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export const SITE = {
   editPost: {
     enabled: true,
     text: "Edit page",
-    url: "https://github.com/NathanHoekstra/nathanhoekstra.nl/edit/main/src/content/blog",
+    url: "https://github.com/NathanHoekstra/nathanhoekstra.nl/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
